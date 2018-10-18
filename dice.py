db481c992de069af056ca481c976e5d907bde5e3
aleksey <zald@yandex.ru>
2018-10-18 22:46:40 +0300

    new

--- dice.py
+++ dice.py
@@ -1,8 +1,8 @@
 import random
 our_chance = 95
-win = 124.4210
+win = 155.5263
 times = int(input('times'))
-bet_size = 120
+bet_size = 150
 for j in range(50):
     summ = 0
     summ2 = 0
@@ -14,6 +14,6 @@ for j in range(50):
     '''print(summ)
     print(summ2)
     print(summ2/summ)'''
-    ref = summ2 * 0.002 + summ2 * 0.0162
-    summ3 = summ + ref
-    print(int((summ3 - summ2)), '\t', ref, '\t', ((summ3 - summ2)/summ2)*100)
+ #   ref = summ2 * 0.002 + summ2 * 0.0162
+    summ3 = summ #+ ref
+    print(int((summ3 - summ2)), '\t', '''ref''', '\t', ((summ3 - summ2)/summ2)*100)
