0f0b84786b0fd21b7c6897090c6547bbb7b210b4
Zald <gavgav@miumiu.xz>
2018-10-21 02:10:13 +0300

    new

--- dice.py
+++ dice.py
@@ -1,9 +1,11 @@
 import random
 our_chance = 95
-win = 155.5263
+win = 103.6842
 times = int(input('times'))
-bet_size = 150
-for j in range(50):
+bet_size = 100
+l = list()
+summroi = 0
+for j in range(10000):
     summ = 0
     summ2 = 0
     for i in range(times):
@@ -14,6 +16,19 @@ for j in range(50):
     '''print(summ)
     print(summ2)
     print(summ2/summ)'''
- #   ref = summ2 * 0.002 + summ2 * 0.0162
-    summ3 = summ #+ ref
-    print(int((summ3 - summ2)), '\t', '''ref''', '\t', ((summ3 - summ2)/summ2)*100)
+    ref = summ2 * 0.002 + summ2 * 0.0137 + summ2 * 0.0006
+    summ3 = summ + ref
+    l.append(int(summ3 - summ2))
+    print(int((summ3 - summ2)), '\t', ref, '\t', ((summ3 - summ2)/summ2)*100)
+    summroi += ((summ3 - summ2)/summ2)*100
+print(l)
+a = sorted(l)
+print(a)
+count = 5
+while a:
+    summ10 = 0
+    for i in range(1000):
+        summ10 += a.pop()
+    print(summ10 / 1000,  '  :', count,'%')
+    count += 10
+print(summroi / 10000)
